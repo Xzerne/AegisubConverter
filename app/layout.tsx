645bda820a554e5996859a0be443cecdc723c2fe
Kujou Kurumi <167000432+Xzerne@users.noreply.github.com>
2025-07-24 12:19:56 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,9 +4,9 @@ import { GeistMono } from 'geist/font/mono'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'srt to ass by Xzerne',
+  description: 'make with some supports with AI',
+  generator: 'Xzerne',
 }
 
 export default function RootLayout({
